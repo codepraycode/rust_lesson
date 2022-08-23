49ebb3c434ced09c5a93cbc67fd565d52a36ec8e
Precious Olusola <preciousolusola16@gmail.com>
2022-08-23 14:21:20 +0100

    Started learning ownership in rust

--- basics_01/target/debug/deps/variables.d
+++
@@ -1,5 +0,0 @@
-C:\Users\User\Rust\basics_01\target\debug\deps\variables.exe: src\main.rs
-
-C:\Users\User\Rust\basics_01\target\debug\deps\variables.d: src\main.rs
-
-src\main.rs:
--- basics_01/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-C:\Users\User\Rust\basics_01\target\debug\variables.exe: C:\Users\User\Rust\basics_01\src\main.rs
