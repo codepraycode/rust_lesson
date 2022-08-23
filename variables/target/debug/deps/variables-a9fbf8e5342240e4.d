f2969e2e2edabfa1d1594f81ea0c9b1135d577bd
Precious Olusola <preciousolusola16@gmail.com>
2022-08-23 12:40:48 +0100

    Now, main branch is for all

---
+++ variables/target/debug/deps/variables-a9fbf8e5342240e4.d
@@ -0,0 +1,5 @@
+c:\Users\User\Rust\variables\target\debug\deps\variables-a9fbf8e5342240e4.rmeta: src\main.rs
+
+c:\Users\User\Rust\variables\target\debug\deps\variables-a9fbf8e5342240e4.d: src\main.rs
+
+src\main.rs:
